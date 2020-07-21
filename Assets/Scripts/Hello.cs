39d64a353d904e080b5014702a78af5f6ee26e1b
Yusuke57 <honey57g@gmail.com>
2020-07-21 14:56:36 +0900

    404ページ作成

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,65 @@
+﻿using DG.Tweening;
+using Extensions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hello : MonoBehaviour
+{
+    [SerializeField] private Transform charParent;
+    
+    private Image[][] squares;
+
+    private const float CHAR_INTERVAL = 0.16f;
+    private const float SQUARE_INTERVAL = 0.005f;
+    private const float SQUARE_DURATION = 0.2f;
+
+    private void Start()
+    {
+        squares = new Image[charParent.childCount][];
+        for (var i = 0; i < squares.Length; i++)
+        {
+            squares[i] = charParent.GetChild(i).GetComponentsInChildren<Image>(true);
+        }
+        
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        for (var i = 0; i < squares.Length; i++)
+        {
+            var charSquares = squares[i];
+            for (var j = 0; j < charSquares.Length; j++)
+            {
+                var square = charSquares[charSquares.Length - j - 1];
+                var rectTransform = square.rectTransform;
+                var pos = rectTransform.anchoredPosition;
+                var alpha = square.color.a;
+                DOTween.Sequence()
+                    .AppendCallback(() =>
+                    {
+                        square.color = new Color(Random.Range(0.6f, 0.7f), 1, Random.Range(0.8f, 1f), alpha);
+                        square = charSquares[charSquares.Length - j - 1];
+                        square.rectTransform.SetPivotWithKeepingPosition(Vector2.right);
+                    })
+                    .Append(square.rectTransform.DOAnchorPosX(1600f, 0))
+                    .Join(square.rectTransform.DOScaleX(12f, 0))
+                    .Join(square.DOFade(1, 0))
+                    .AppendInterval(0.2f)
+                    .AppendInterval(CHAR_INTERVAL * i + SQUARE_INTERVAL * j)
+                    .Append(square.rectTransform.DOAnchorPosX(pos.x, SQUARE_DURATION).SetEase(Ease.OutQuart))
+                    .Join(square.rectTransform.DOScaleX(1, SQUARE_DURATION).SetEase(Ease.OutQuart))
+                    .Join(square.DOFade(alpha, SQUARE_DURATION).SetEase(Ease.OutQuart))
+
+                    .AppendInterval(0.4f)
+                    .Append(square.DOColor(new Color(1, 1, 1, alpha + 0.2f), 0.05f).SetLoops(2, LoopType.Yoyo))
+                    .AppendInterval(0.4f)
+                    .AppendInterval(SQUARE_INTERVAL * (charSquares.Length - j))
+
+                    .AppendInterval(Random.Range(0f, 0.2f))
+                    .Append(square.rectTransform.DOAnchorPosX(-100f, SQUARE_DURATION / 2))
+                    .Join(square.DOFade(0, SQUARE_DURATION / 2));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Page404.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Extensions;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Page404 : MonoBehaviour
+{
+    [SerializeField] private RectTransform compass;
+    [SerializeField] private Image[] images404;
+    [SerializeField] private RectTransform background;
+
+    float angle = 0f;
+    float duration = 0f;
+    int seed = Environment.TickCount;
+
+    private void Awake()
+    {
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        DOTween.Sequence()
+            .OnStart(() =>
+            {
+                foreach (var image in images404) image.SetAlpha(0);
+                background.localScale = Vector3.one * 2f;
+                DOTween.Sequence()
+                    .Append(background.DOScale(1, 1f))
+                    .Append(images404[0].DOFade(1, 0.4f))
+                    .Join(images404[1].DOFade(1, 0.4f))
+                    .Join(images404[2].DOFade(1, 0.4f))
+                    .OnComplete(() =>
+                    {
+                        background.DOScale(1.04f, 1f)
+                            .SetEase(Ease.InOutQuad)
+                            .SetLoops(-1, LoopType.Yoyo);
+                    });
+            })
+            .AppendCallback(() =>
+            {
+                compass.DOKill();
+                compass.DOLocalRotate(Vector3.forward * Random.Range(-1200, 1200), 1.4f)
+                    .SetEase(Ease.Linear)
+                    .SetRelative(true);
+            })
+            .AppendInterval(Random.Range(0.4f, 1.4f))
+            .SetLoops(-1);
+    }
+}
